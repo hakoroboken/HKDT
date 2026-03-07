423c7f2b9ea91f8552314e22443b6e03e2f7e265
motii8128 <default270robo@gmail.com>
2026-03-07 16:10:41 +0900

    [update] FastSerialを組み込み型に変更

--- Assets/HKDT/Connection/FastSerial.cs
+++ Assets/HKDT/Connection/FastSerial.cs
@@ -10,166 +10,151 @@ using System.IO.Ports;
 // ROS2
 using ROS2;
 
-public class FastSerial : MonoBehaviour
+namespace HKDT.Connection
 {
-    // シリアル通信のパラメータ
-    [Header("シリアル通信設定")]
-    public string portName="/dev/ttyACM0";
-    public int baudRate = 115200;
-    public int readTimeout = 10;
-    public int writeTimeout = 10;
-
-    // ROS2パラメータ
-    [Header("ROS2設定")]
-    public string nodeName = "FastSerial";
-    public string writeTopicName = "/serial/write";
-    public string readTopicName = "/serial/read";
-
-    // シリアルポート
-    private SerialPort serialPort;
-    // 受信を行うスレッド
-    private Thread readThread;
-
-    // ROS2
-    private ROS2UnityComponent ros2unity;
-    private ROS2Node node;
-    // シリアル通信で受信したデータをROS2ネットワークに送信する
-    private IPublisher<std_msgs.msg.ByteMultiArray> readDataPublisher;
-    // シリアル通信で送信するデータをROS2ネットワークから受信する
-    private ISubscription<std_msgs.msg.ByteMultiArray> writeDataSubscriber;
-
-    ConcurrentQueue<byte[]> writeData;
-
-    public void Start()
+    public class FastSerial
     {
-        ros2unity = GetComponent<ROS2UnityComponent>();
-    }
+        // シリアル通信のパラメータ
+        [Header("シリアル通信設定")]
+        private string portName="/dev/ttyACM0";
+        private int baudRate = 115200;
+        private int readTimeout = 10;
+        private int writeTimeout = 10;
 
-    public void Update()
-    {
-        if(ros2unity.Ok())
+        // シリアルポート
+        private SerialPort serialPort;
+        // 受信を行うスレッド
+        private Thread readThread;
+
+        // ROS2
+        // シリアル通信で受信したデータをROS2ネットワークに送信する
+        private IPublisher<std_msgs.msg.UInt8MultiArray> readDataPublisher;
+        private IPublisher<std_msgs.msg.Bool> statusPublisher;
+        // シリアル通信で送信するデータをROS2ネットワークから受信する
+        private ISubscription<std_msgs.msg.UInt8MultiArray> writeDataSubscriber;
+
+        ConcurrentQueue<byte[]> writeData;
+
+        public FastSerial(string port_name="/dev/ttyACM0", int baud_rate=115200, int read_timeout=10, int write_timeout=10)
         {
-            if(node == null)
-            {
-                Debug.Log($"[{nodeName}]ノードを初期化します");
-                InitializeNode();
-                Debug.Log($"[{nodeName}]シリアル通信を開始します");
-                OpenSerial();
-            }
+            portName = port_name;
+            baudRate = baud_rate;
+            readTimeout = read_timeout;
+            writeTimeout = write_timeout;
         }
-        else
+
+        /// <summary>
+        /// PublisherとSubscriberを初期化しシリアル通信を開始します
+        /// </summary>
+        /// <param name="node">ノード</param>
+        /// <param name="write_topic_name">シリアル送信するデータ</param>
+        /// <param name="read_topic_name">シリアル受信したデータ</param>
+        /// /// <param name="status_topic_name">シリアル通信の状態</param>
+        /// <returns>初期化成功したらtrue、失敗ならfalse</returns>
+        public bool Start(ROS2Node node, string write_topic_name, string read_topic_name, string status_topic_name)
         {
-            Debug.LogError($"[{nodeName}]ROS2が正常でない");
+            readDataPublisher = node.CreatePublisher<std_msgs.msg.UInt8MultiArray>(read_topic_name);
+            writeDataSubscriber = node.CreateSubscription<std_msgs.msg.UInt8MultiArray>(write_topic_name, WriteDataCallback);
+            statusPublisher = node.CreatePublisher<std_msgs.msg.Bool>(status_topic_name);
+
+            return OpenSerial();
         }
-    }
 
-    public void OnDestroy()
-    {
-        CloseSerial();
-        Debug.Log($"[{nodeName}]シリアル通信を終了しました");
-        return;
-    } 
-
-    /// <summary>
-    /// ROS2のパブリッシャーとサブスクライバを初期化する
-    /// </summary>
-    private void InitializeNode()
-    {
-        node = ros2unity.CreateNode(nodeName);
-
-        readDataPublisher = node.CreatePublisher<std_msgs.msg.ByteMultiArray>(readTopicName);
-
-        writeDataSubscriber = node.CreateSubscription<std_msgs.msg.ByteMultiArray>(
-            writeTopicName,
-            WriteDataCallback
-        );
-    }
-
-    /// <summary>
-    /// ROS2サブスクライバがデータを受信した際に実行する関数
-    /// </summary>
-    /// <param name="msg"></param>
-    private void WriteDataCallback(std_msgs.msg.ByteMultiArray msg)
-    {
-        writeData.Enqueue(msg.Data);
-    }
-
-    /// <summary>
-    /// シリアル通信を開始する
-    /// </summary>
-    private void OpenSerial()
-    {
-        try
+        private void WriteDataCallback(std_msgs.msg.UInt8MultiArray msg)
         {
-            serialPort = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One)
-            {
-                NewLine = "\n",
-                ReadTimeout = readTimeout,
-                WriteTimeout = writeTimeout
-            };
-
-            serialPort.Open();
-            
-            readThread = new Thread(SerialCallback);
-            readThread.Start();
-
-            writeData = new();
+            writeData.Enqueue(msg.Data);
         }
-        catch(Exception ex)
-        {
-            Debug.LogError($"[{nodeName}] シリアル通信を開始できませんでした。: {ex.Message}");
-        }
-    }
 
-    /// <summary>
-    /// シリアル通信を終了する
-    /// </summary>
-    public void CloseSerial()
-    {
-        if(readThread != null && readThread.IsAlive)readThread.Join();
-
-        if(serialPort != null && serialPort.IsOpen)serialPort.Close();
-    }
-
-    /// <summary>
-    /// シリアル通信で受信した際に実行する関数
-    /// </summary>
-    private void SerialCallback()
-    {
-        while(true)
+        private bool OpenSerial()
         {
             try
             {
-                byte[] buffer = new byte[256];
-                int readSize = serialPort.Read(buffer, 0, 256);
-
-                if(readSize > 0)
+                serialPort = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One)
                 {
-                    byte[] read_data = new byte[readSize];
-                    Array.Copy(buffer, read_data, readSize);
+                    NewLine = "\n",
+                    ReadTimeout = readTimeout,
+                    WriteTimeout = writeTimeout
+                };
 
-                    var read_data_msg = new std_msgs.msg.ByteMultiArray
-                    {
-                        Data = read_data
-                    };
+                serialPort.Open();
+                
+                readThread = new Thread(SerialCallback);
+                readThread.Start();
 
-                    readDataPublisher.Publish(read_data_msg);
+                writeData = new();
 
-                    if(writeData.TryDequeue(out var write_buffer))
-                    {
-                        serialPort.Write(write_buffer, 0, write_buffer.Length);
-                    }
-                }
-            }
-            catch(TimeoutException)
-            {
-                // Timeout時はなにもしない
-                // Debug.Log($"[{nodeName}] たいむあうと〜");
+                return true;
             }
             catch(Exception ex)
             {
-                Debug.LogError($"[{nodeName}] サブスレッドエラー：{ex.Message}");
+                Debug.LogError($"シリアル通信を開始できませんでした。: {ex.Message}");
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// シリアル通信を終了する
+        /// </summary>
+        public void CloseSerial()
+        {
+            if(readThread != null && readThread.IsAlive)readThread.Join();
+
+            if(serialPort != null && serialPort.IsOpen)serialPort.Close();
+        }
+
+        private void SerialCallback()
+        {
+            while(true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[256];
+                    int readSize = serialPort.Read(buffer, 0, 256);
+
+                    if(readSize > 0)
+                    {
+                        byte[] read_data = new byte[readSize];
+                        Array.Copy(buffer, read_data, readSize);
+
+                        var read_data_msg = new std_msgs.msg.UInt8MultiArray
+                        {
+                            Data = read_data
+                        };
+
+                        readDataPublisher.Publish(read_data_msg);
+
+                        if(writeData.TryDequeue(out var write_buffer))
+                        {
+                            serialPort.Write(write_buffer, 0, write_buffer.Length);
+                        }
+
+                        var status_msg = new std_msgs.msg.Bool
+                        {
+                            Data = true
+                        };
+                        statusPublisher.Publish(status_msg);
+                    }
+                }
+                catch(TimeoutException)
+                {
+                    // Timeout時はなにもしない
+                    // Debug.Log($"[{nodeName}] たいむあうと〜");
+                    var status_msg = new std_msgs.msg.Bool
+                    {
+                        Data = false
+                    };
+                    statusPublisher.Publish(status_msg);
+                }
+                catch (Exception)
+                {
+                    var status_msg = new std_msgs.msg.Bool
+                    {
+                        Data = false
+                    };
+                    statusPublisher.Publish(status_msg);
+                }
             }
         }
     }
-}
+}// end of namespace
